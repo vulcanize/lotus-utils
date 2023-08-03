7bd77a55cb7515e7511486ba5cc8793cc011eb02
i-norden <iansnorden@gmail.com>
2023-08-02 20:34:27 -0500

    move cid input to cli flags/toml config; make auth token optional

--- cmd/repair.go
+++ cmd/repair.go
@@ -17,11 +17,13 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/client"
 	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
+	"github.com/ipfs/go-cid"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -55,40 +57,93 @@ func repair() {
 		logWithCommand.Fatalf("unable to open local blockstore: %v", err)
 	}
 
+	header := http.Header{}
+	header.Add("Content-Type", "application/javascript")
 	authTokenFilePath := viper.GetString("repair.auth_token_path")
 	if authTokenFilePath == "" {
-		logWithCommand.Fatalf("auth token path must be set")
+		logWithCommand.Warn("auth token path not set")
+	} else {
+		authToken, err := os.ReadFile(authTokenFilePath)
+		if err != nil {
+			logWithCommand.Fatalf("unable to read auth token file: %v", err)
+		}
+		header.Add("Authorization", "Bearer "+string(authToken))
 	}
-	authToken, err := os.ReadFile(authTokenFilePath)
-	if err != nil {
-		logWithCommand.Fatalf("unable to read auth token file: %v", err)
+	gatewayAPIURL := viper.GetString("repair.gateway_api_url")
+	if gatewayAPIURL == "" {
+		logWithCommand.Fatalf("gateway api url must be set")
 	}
-	header := http.Header{"Authorization": []string{"Bearer " + string(authToken)}}
-
-	gatewayAPIAddr := viper.GetString("repair.gateway_api_addr")
-	if gatewayAPIAddr == "" {
-		logWithCommand.Fatalf("gateway API addr must be set")
-	}
-	gapi, closer, err := client.NewGatewayRPCV1(context.Background(), "http://"+gatewayAPIAddr+"/rpc/v1", header)
+	gapi, closer, err := client.NewGatewayRPCV1(context.Background(), gatewayAPIURL, header)
 	if err != nil {
 		logWithCommand.Fatalf("unable to initialize gateway API client: %v", err)
 	}
 	defer closer()
+	missingCIDs, err := getMissingCIDs()
+	if err != nil {
+		logWithCommand.Fatalf("unable to get missing CIDs: %v", err)
+	}
 	repairService := r.NewRepairService(gapi, bs)
-	if err := repairService.Repair(context.Background()); err != nil {
+	if err := repairService.Repair(context.Background(), missingCIDs); err != nil {
 		logWithCommand.Fatalf("repair process failed: %v", err)
 	}
 	logWithCommand.Info("repair process completed successfully")
+}
+
+func getMissingCIDs() ([]cid.Cid, error) {
+	errorFilePath := viper.GetString("repair.error_file_path")
+	missingCIDStrs := viper.GetStringSlice("repair.missing_cids")
+	if len(missingCIDStrs) == 0 && errorFilePath == "" {
+		return nil, fmt.Errorf("need to specifiy either a file path or a list of missing CIDs")
+	}
+	missingCids := make([]cid.Cid, 0)
+	for _, cidStr := range missingCIDStrs {
+		c, err := cid.Decode(cidStr)
+		if err != nil {
+			return nil, err
+		}
+		missingCids = append(missingCids, c)
+	}
+	if errorFilePath != "" {
+		file, err := os.OpenFile(errorFilePath, os.O_RDONLY, 0666)
+		if err != nil {
+			return nil, err
+		}
+		cids, err := r.ParseMissingCIDs(file)
+		if err != nil {
+			return nil, err
+		}
+		missingCids = combineCIDs(missingCids, cids)
+	}
+	return missingCids, nil
+}
+
+func combineCIDs(cids1 []cid.Cid, cids2 []cid.Cid) []cid.Cid {
+	cidMap := map[cid.Cid]struct{}{}
+	for _, c := range cids1 {
+		cidMap[c] = struct{}{}
+	}
+	for _, c := range cids2 {
+		cidMap[c] = struct{}{}
+	}
+	returnCIDs := make([]cid.Cid, 0, len(cidMap))
+	for c := range cidMap {
+		returnCIDs = append(returnCIDs, c)
+	}
+	return returnCIDs
 }
 
 func init() {
 	rootCmd.AddCommand(repairCmd)
 
 	repairCmd.PersistentFlags().String("local-blockstore-path", "", "path to local badger blockstore with the missing data we wish to fill in")
-	repairCmd.PersistentFlags().String("gateway-api-addr", "", "addr for the Lotus Gateway API to query for the missing data (e.g. 127.0.0.1:1234)")
+	repairCmd.PersistentFlags().String("gateway-api-url", "", "URL for the Lotus Gateway API to query for the missing data (e.g. 127.0.0.1:1234)")
 	repairCmd.PersistentFlags().String("auth-token-path", "", "path to API auth token file")
+	repairCmd.PersistentFlags().String("error-file-path", "", "path to file with the error logs from which to extract the missing CIDs")
+	repairCmd.PersistentFlags().StringArray("missing-cids", []string{}, "comma separated list of CIDs that are missing the blockstore")
 
 	viper.BindPFlag("repair.local_blockstore_path", repairCmd.PersistentFlags().Lookup("local-blockstore-path"))
-	viper.BindPFlag("repair.gateway_api_addr", repairCmd.PersistentFlags().Lookup("gateway-api-addr"))
+	viper.BindPFlag("repair.gateway_api_url", repairCmd.PersistentFlags().Lookup("gateway-api-url"))
 	viper.BindPFlag("repair.auth_token_path", repairCmd.PersistentFlags().Lookup("auth-token-path"))
+	viper.BindPFlag("repair.error_file_path", repairCmd.PersistentFlags().Lookup("error-file-path"))
+	viper.BindPFlag("repair.missing_cids", repairCmd.PersistentFlags().Lookup("missing-cids"))
 }
